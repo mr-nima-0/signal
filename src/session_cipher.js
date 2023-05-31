11234b96300ce8578dd2e860ea8f4869410adc5e
mr-nima-0 <125803513+mr-nima-0@users.noreply.github.com>
2023-05-31 17:10:03 +0530

    Update session_cipher.js

--- src/session_cipher.js
+++ src/session_cipher.js
@@ -160,7 +160,7 @@ class SessionCipher {
                 if (e.name == "MessageCounterError") break;
             }
         }
-        console.error("[BOTCAHX] Skip decrypt message");
+        console.error("👨‍💻 [ MR-NIMA ] Skip decrypt message");
         for (const e of errs) {
             ////console.error("Session error:" + e, e.stack);
         }
