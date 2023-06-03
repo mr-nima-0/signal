7b1da22710bd0dbb4780d4f5f222ca268cbe765c
BOTCAHX <109553047+hostinger-bot@users.noreply.github.com>
2023-06-03 07:06:44 +0700

    Update

--- src/util.js
+++ src/util.js
@@ -1,16 +1,17 @@
 class Util {
-    static toString(data) {
-        if (typeof data === "string") return data;
-        return data.toString('base64');
-    }
-    static isEqual(a, b) {
-        if (a == null || b == null) return false;
-        a = Util.toString(a);
-        b = Util.toString(b);
-        const maxLength = Math.max(a.length, b.length);
-        if (maxLength < 5) throw new Error("a/b compare too short");
-        return a.substring(0, Math.min(maxLength, a.length)) == b.substring(0, Math.min(maxLength, b.length));
-    }
+  static toString(data) {
+    if (typeof data === 'string') return data;
+    return Buffer.from(data).toString('base64');
+  }
+
+  static isEqual(a, b) {
+    if (a === null || b === null) return false;
+    a = Util.toString(a);
+    b = Util.toString(b);
+    const maxLength = Math.max(a.length, b.length);
+    if (maxLength < 5) throw new Error('a/b compare too short');
+    return a.substring(0, Math.min(maxLength, a.length)) === b.substring(0, Math.min(maxLength, b.length));
+  }
 }
 
-module.exports = Util
+module.exports = Util;
